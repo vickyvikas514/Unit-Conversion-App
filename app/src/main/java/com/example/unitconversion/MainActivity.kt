cb1586c4a4a3313e45d5fa5dfeaf81ce397a9304
Vikas Chaudhary <vikaschaudhary8286@gmail.com>
2022-09-22 20:06:44 +0530

    landscape rotaion is added

--- app/src/main/java/com/example/unitconversion/MainActivity.kt
+++ app/src/main/java/com/example/unitconversion/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.unitconversion
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.getSystemService
 import com.example.unitconversion.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import kotlin.Double
@@ -10,9 +15,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.imageView2.setOnClickListener { calcconv() }
+        binding.yourReadingEditText.setOnKeyListener { view, keyCode, _ ->
+            handlekey(view, keyCode)
+        }
+    }
+    private fun handlekey(view: View,keycode:Int):Boolean{
+        if(keycode== KeyEvent.KEYCODE_ENTER){
+            val inputMethodManager=getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+            return true
+        }
+        return false
     }
 
     private fun calcconv() {
